fd510f66e808d572560020b98b88d443fa5cdffa
fear1505 <80864390+fear1505@users.noreply.github.com>
2021-07-11 21:18:21 -0300

    Cambio 4
    borre el /porfolio/servicios/contacto

--- header/header.js
+++ header/header.js
@@ -10,15 +10,15 @@ function headerComponent(el) {
   <div class="header__menu-hamburg-ventana">
   <img class="header__menu-hamburg-ventana__x" src="./img-m4/x-ventana.png" alt="">
   <div class="header__menu-hamburg-ventana__contenido">
-    <a class="header__menu-hamburg-ventana__contenido__psc" href="./porfolio/porfolio.html">Portafolio</a>
-    <a class="header__menu-hamburg-ventana__contenido__psc" href="./servicios/servicios.html">Servicios</a>
-    <a class="header__menu-hamburg-ventana__contenido__psc" href="./contacto/contacto.html">Contacto</a>
+    <a class="header__menu-hamburg-ventana__contenido__psc" href="./porfolio.html">Portafolio</a>
+    <a class="header__menu-hamburg-ventana__contenido__psc" href="./servicios.html">Servicios</a>
+    <a class="header__menu-hamburg-ventana__contenido__psc" href="./contacto.html">Contacto</a>
   </div>
   </div>
   <div class="header__desktop">
-      <a class="header__desktop_menu" href="./porfolio/porfolio.html">Portafolio</a>
-      <a class="header__desktop_menu" href="./servicios/servicios.html">Servicios</a>
-      <a class="header__desktop_menu" href="./contacto/contacto.html">Contacto</a>
+      <a class="header__desktop_menu" href="./porfolio.html">Portafolio</a>
+      <a class="header__desktop_menu" href="./servicios.html">Servicios</a>
+      <a class="header__desktop_menu" href="./contacto.html">Contacto</a>
   </div>
 </header>`;
   el.appendChild(headerEl);
